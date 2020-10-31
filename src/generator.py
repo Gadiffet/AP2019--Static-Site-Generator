f091ef83a1f5d070e45040c3f22dbb435023ad8a
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-10-31 17:56:07 +0100

    feat(template):Add template + Style

--- src/generator.py
+++ src/generator.py
@@ -1,45 +1,40 @@
-import markdown
+from markdown2 import markdown
+from jinja2 import Environment, FileSystemLoader
+from json import load
 import menu
 import os
 import sys
 import subprocess
 from shutil import copyfile
 
-
-def convertToHTML(convertToHTML):
-    exts = ['markdown.extensions.extra', 'markdown.extensions.codehilite',
-            'markdown.extensions.tables', 'markdown.extensions.toc']
-
-    html = '''
-<html lang="fr-en">
-    <head>
-        <meta content="text/html; charset=utf-8" http-equiv="content-type" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    </head>
-    <body>
-        %s
-    </body>
-</html>
-           '''
-
-    ret = markdown.markdown(convertToHTML, extensions=exts)
-    return html % ret
-
+env = Environment(loader=FileSystemLoader(searchpath='./'))
+template = env.get_template('template.html')
 
 def oneFile(markdownFile, htmlFile):
 
     if markdownFile.endswith('.md') and htmlFile.endswith('html'):
 
-        infile = open(markdownFile, 'r')
-        md = infile.read()
-        infile.close()
+        with open(markdownFile, 'r') as infile :
+            site = markdown(
+                infile.read(),
+                extras= ['tables', 'codehilite', 'toc', 'nl2br', 'fenced-code-blocks', 'abbr', 'attr_list', 'def_list', 'footnotes', "md_in_html"]
+            )
+            infile.close()
+
+        with open("config.json") as config_file:
+            config = load(config_file)
 
         if os.path.exists(htmlFile):
             os.remove(htmlFile)
-
-        outfile = open(htmlFile, 'x')
-        outfile.write(convertToHTML(md))
-        outfile.close()
+        
+        with open(htmlFile, 'x') as outfile :
+            outfile.write(
+                template.render(
+                    article = site,
+                    title = config['title']
+                )
+            )
+            outfile.close()
     else:
         print("\n*----*\n ")
         print("*-- Votre fichier comporte une extension differente du MarkDown ou de l'HTML --*")
--- src/start.py
+++ src/start.py
@@ -1,9 +1,9 @@
 import sys
 import subprocess
-import menu
 
 subprocess.check_call([sys.executable, "-m", "pip",
                        "install", "-r", "../requirement.txt"])
 
 if __name__ == '__main__':
+    import menu
     menu.menuPrincipal()
