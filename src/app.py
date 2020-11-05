177f70e973dd6eb3fcfd369e9e5d893d2070e264
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-11-05 15:11:03 +0100

    feat(deploy): Add test.html

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='conversion')
 
 def html(htmlFile):
-    return './conversion/' + htmlFile
+    fileHTML = htmlFile
+    return fileHTML
+
+@app.route('/')
+def index(fileHTML):
+    return render_template(fileHTML); 
 
 if __name__ == '__main__':
     app.run()
