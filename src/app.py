edb5f96b4a94475abe7f975f2cb1ecc363556cd9
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-11-05 13:54:03 +0100

    test

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def template():
-    return render_template('template.html')
+def hello_world():
+    return 'hello_world'
 
 if __name__ == '__main__':
     app.run()
