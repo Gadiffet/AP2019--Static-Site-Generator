ffac5579c3c45f06664c6b0fff347b15a13c27f5
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-11-05 15:34:34 +0100

    fix(deploy):fix issues to deploy

--- src/app.py
+++ src/app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='conversion')
 
-def html(htmlFile):
-    fileHTML = htmlFile
-    return fileHTML
-
 @app.route('/')
-def index(fileHTML):
-    return render_template(fileHTML); 
+def html(htmlFile):
+    return render_template(htmlFile); 
 
 if __name__ == '__main__':
     app.run()
