dc8b0e360460c3a1e29d6c439acbac8e8bb90f70
T.Gadiffet <theo.gadiffet@leroymerlin.fr>
2020-11-05 11:01:41 +0100

    test

--- src/app.py
+++ src/app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-ENV = 'dev'
-
-if ENV == 'dev':
-    app.debug = True
-else:
-    app.debug = False
-
 @app.route('/')
 def template():
     return render_template('template.html')
